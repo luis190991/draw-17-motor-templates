6a33cf17239e4c6dbae1840cd54358474f1bc590
Luis Ramírez <luis@MacBook-Pro-de-Luis.local>
2017-02-09 20:45:19 -0700

    Inicio de proyecto.

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require("http");
+
+
+http.createServer(function(req, res){
+  res.writeHead(200, {"content-type":"text/html"});
+  res.write("Hola Mundo!!");
+  res.end();
+}).listen(8888);
